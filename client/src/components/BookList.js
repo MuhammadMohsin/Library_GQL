a110a31e193071cdcaad83d3ed77e86d63c6fde2
MuhammadMohsin <mmohsinkhalid@outlook.com>
2020-01-13 18:04:58 +0500

    configure Apollo and integrate get all books query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,16 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components'
 
-class BookList extends Component {
-    render(){
-        return (
-            <div>
-                <h1>Books</h1>
-                <ul>
-                    <li>Book 1</li>
-                </ul>
-            </div>
-        )
-    }
+const getBooks = gql`
+        {
+            books {
+                id,
+                name,
+                genre,
+                author {
+                    name
+                }
+            }
+        }
+    `
+function BookList() {
+
+    const booksData = useQuery(getBooks);
+    if (booksData?.loading)
+        return null;
+    const { books } = booksData.data;
+
+    return (
+        <>
+            <h1>Books</h1>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Genre</th>
+                        <th>Author</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map(book => {
+                        return (
+                            <tr key={book.id} className="book-info">
+                                <td>{book.name || '-'} </td>
+                                <td>{book.genre || '-'}</td>
+                                <td>{book.author?.name || '-'}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+        </>
+    )
 }
+
+const Table = styled.table`
+    border: 1px solid black;
+    border-collapse: collapse;
+    width: 65vw;
+    min-width: 500px;
+    thead {
+        background-color: #901e1e;
+        color: white;
+        text-transform: uppercase;
+    }
+    td,th {
+        padding: 10px;
+        border: 1px solid black;
+    }
+    tr.book-info:hover{
+        background-color: #d8d8d8;
+    }
+`
 
 export default BookList;
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import BookList from './components/BookList';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { client } from './config';
 
-ReactDOM.render(<BookList />, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <BookList />
+    </ApolloProvider>,
+    document.getElementById('root'));
