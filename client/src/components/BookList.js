168cfc1355440efeb1969dd28acd17c1ab011749
MuhammadMohsin <mmohsinkhalid@outlook.com>
2020-01-13 16:43:11 +0500

    Add Booklist component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Books</h1>
+                <ul>
+                    <li>Book 1</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default BookList;
